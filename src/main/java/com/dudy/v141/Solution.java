07e9b99224ed4429cc8a7f0cf209bd09caf0cffa
dudy0127 <dudy0127@gmail.com>
2018-07-31 15:00:48 +0800

    19,141, add readme

---
+++ src/main/java/com/dudy/v141/Solution.java
@@ -0,0 +1,47 @@
+package com.dudy.v141;
+
+/**
+
+ 给定一个链表，判断链表中是否有环。
+
+ 进阶：
+ 你能否不使用额外空间解决此题？
+ TODO:环的长度
+ TODO:环的入口
+
+ 解题思路:
+ 1.双指针
+ 2.Hash表判断是否存在，时间复杂度0(n)
+
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+
+
+        if(head == null) return false;
+
+        if(head.next == null || head.next.next == null) return false;
+
+        ListNode preNode = head;
+        ListNode curNode = head;
+
+        while (curNode.next != null && curNode.next.next != null){
+            preNode = preNode.next;
+            curNode = curNode.next.next;
+            if(preNode == curNode){
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/dudy/v19/Solution.java
@@ -0,0 +1,56 @@
+package com.dudy.v19;
+
+/**
+ *
+ 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+
+ 示例：
+
+ 给定一个链表: 1->2->3->4->5, 和 n = 2.
+
+ 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ 说明：
+
+ 给定的 n 保证是有效的。
+
+ 进阶：
+
+ 你能尝试使用一趟扫描实现吗？
+ */
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+ }
+
+/**
+ * 解题思路:
+ * 双指针解法(前指针，后指针)，先让前指针走n个步，再让两个在指针同时后移，直到前指针到达尾部，此时，后指针的下一个节点就是要被删除的节点了。
+ *
+ */
+public  class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+
+        ListNode preNode = head;
+        ListNode curNode = head;
+
+        for (int i = 0;i<n;i++){
+            curNode = curNode.next;
+        }
+
+        if(curNode == null){
+            return preNode.next;
+        }
+
+
+        while (curNode.next != null){
+            curNode = curNode.next;
+            preNode = preNode.next;
+        }
+
+        preNode.next = preNode.next.next;
+
+        return head;
+    }
+}
