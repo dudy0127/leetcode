dccf0fb3b692106ac586e1a9eb5b93ce44361388
Dudy <1796244932@qq.com>
2019-02-14 23:59:04 +0800

    92

--- src/main/java/io/github/dudy/leecode/vtemp/Solution.java
+++ src/main/java/io/github/dudy/leecode/vtemp/Solution.java
@@ -25,39 +25,23 @@ class ListNode {
 class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
 
-        if (head == null || head.next == null) {
-            return head;
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode mPre=dummy,nNext=null,curr=null,next=null,prev=null;
+
+        for(int i=0;i<m-1;i++){
+            mPre=mPre.next; // 查找m点的前节点
         }
-
-        // 找到开始的节点
-        int i ;
-        ListNode newHead = null;
-        ListNode startNode = null;
-
-        for (i = 0; i < m; i++) {
-            startNode = head.next;
+        curr=mPre.next; // 当前节点
+        for(int i=0;i<=n-m;i++){
+            next=curr.next; // 下一个节点
+            curr.next=prev; //
+            prev=curr;
+            curr=next;
+            nNext=next;
         }
-
-        ListNode tempStartNode = startNode;
-
-        // 从开始节点反转,反转出新的链表
-        while (startNode.next != null && i< n){
-
-            ListNode temp = startNode.next;
-
-            startNode.next = newHead;
-
-            newHead = startNode;
-
-            startNode = temp;
-            i++;
-        }
-
-
-        // 用中间这段替换掉
-
-
-
-        return head;
+        mPre.next.next=nNext;
+        mPre.next=prev;
+        return dummy.next;
     }
 }
